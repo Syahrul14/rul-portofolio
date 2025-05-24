7c1128817e8d7de0143cebd5d905825934c35309
Syahrul Rizal <78249683+Syahrul14@users.noreply.github.com>
2025-05-24 14:12:24 +0700

    add location

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -11,24 +11,28 @@ const WorkExperience = () => {
       position: "Programmer",
       period: "Dec 2024 - Present, Fulltime",
       description: "I developed the Yi Maintenance Data Trouble Machine web application using Laravel and a PostgreSQL database. In this project, I implemented data visualization features using Chart.js to display machine trouble graphs, and created a detailed Chokotei data page for easier issue analysis. I also designed and built an Oil Inventory module, added a feature for recording oil PM refill data, and generated oil usage reports for monitoring and evaluation purposes.",
+      location: 'Serang',
     },
     {
       company: "PT Tribuana Global Group",
       position: "Full Stack Developer",
       period: "May 2023 - Aug 2024, Fulltime",
       description: "Developed and maintained a CMS with CodeIgniter 3, built HRIS and ERP applications using Laravel and Vue.js, and designed new modules for these systems.",
+      location: 'Jakarta',
     },
     {
       company: "Digitalent Scholarship Kominfo Batch 1 X Progate",
       position: "Frontend Developer - Progate",
       period: "Mar 2023 - Apr 2023, Internship",
       description: "Mastered responsive web design, applied flexbox, and developed websites collaboratively in a team.",
+      location: 'Remote',
     },
     {
       company: "Yayasan Anak Bangsa Bisa",
-      position: "Frontend Engineer - Generasi GIGIH 2.0 by YABB & GoTo Mar 2022",
+      position: "Frontend Engineer - Generasi GIGIH 2.0 by YABB & GoTo",
       period: "Mar 2022 - Juli 2022, Internship",
       description: "Developed the Capstone Project Jofer utilizing React JS, focusing on creating a dynamic and interactive user interface, while applying best practices in front-end development to ensure scalability and maintainability.",
+      location: 'Remote',
     },
   ];
 
@@ -85,19 +89,19 @@ const WorkExperience = () => {
 
 
   return (
-    <section id="experience" className="container mx-auto py-10 px-4 mt-8">
+    <section id="experience" className="container px-4 py-10 mx-auto mt-8">
       <motion.div
         variants={headingVariants}
         initial="hidden"
         animate={controls}
       >
-        <h2 className="text-3xl text-rose-500 font-bold mb-8">🏢 Work Experience</h2>
-        <p className="text-white mb-10 lg:w-2/5">
+        <h2 className="mb-8 text-3xl font-bold text-rose-500">🏢 Work Experience</h2>
+        <p className="mb-10 text-white lg:w-2/5">
           I have experience working as a software engineer, including projects in web application development as a full-stack and frontend developer.
         </p>
       </motion.div>
       <motion.div
-        className="relative border-l border-gray-300 ml-4"
+        className="relative ml-4 border-l border-gray-300"
         variants={containerVariants}
         initial="hidden"
         animate={controls}
@@ -110,10 +114,10 @@ const WorkExperience = () => {
             ref={(el) => (itemRefs.current[index] = el)}
             data-index={index}
           >
-            <div className="absolute w-4 h-4 bg-rose-500 rounded-full -left-8 top-0 transform -translate-x-1/2 -translate-y-1/2 border border-white"></div>
+            <div className="absolute top-0 w-4 h-4 transform -translate-x-1/2 -translate-y-1/2 border border-white rounded-full bg-rose-500 -left-8"></div>
             <p className="text-sm text-yellow-500">{exp.period}</p>
             <h3 className="text-xl font-semibold text-white">{exp.position}</h3>
-            <h4 className="text-lg font-medium text-blue-500">{exp.company}</h4>
+            <h4 className="text-lg font-medium text-blue-500">{exp.company}, ({exp.location})</h4>
             <p className="mt-2 text-gray-300">{exp.description}</p>
           </motion.div>
         ))}
